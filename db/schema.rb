7f6631083fca646f33c71c99fe56192264d193cd
SonOfBattles1 <peter.shuck@comcast.net>
2023-10-21 12:43:26 -0400

    Post resource spun up and associations set up with User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_21_162605) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_21_163414) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.integer "user_id", null: false
+    t.datetime "posted_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "name"
@@ -20,4 +30,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_21_162605) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
