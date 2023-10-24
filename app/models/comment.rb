26367ef37d9bbf91c77403c9f265d86a57e529cd
SonOfBattles1 <peter.shuck@comcast.net>
2023-10-23 22:50:14 -0400

    Added author and author_average_method to Post and Comment as_json

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,17 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
+
+  def as_json(options={})
+    options[:methods] = %i(author author_average_rating)
+    super
+  end
+
+  def author
+    user.name
+  end
+
+  def author_average_rating
+    user.average_rating
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,11 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   belongs_to :user
 
+  def as_json(options={})
+    options[:methods] = %i(number_of_comments author author_average_rating)
+    super
+  end
+
   def number_of_comments
     comments.length
   end
